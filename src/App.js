1dd640ac443cab539b2f3687767276d1982f2979
Ilija Panov <ilijapanov83@gmail.com>
2021-06-09 22:53:44 +0200

    completed react poc

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Header from './components/header/header'
+import Counter from './components/Counter/Counter';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Counter />
+    </div>
+  );
+}
+
+export default App;
